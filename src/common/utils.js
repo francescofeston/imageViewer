da6edac0be8512defd90411f93aefb4c0161d058
francescofeston <francescofeston@gmail.com>
2020-10-23 15:07:20 +0200

    added utils.js

---
+++ src/common/utils.js
@@ -0,0 +1,5 @@
+export const constants = {
+	userInfoUrl: 'https://graph.instagram.com/me?fields=id,username,media_count',
+	userMediaUrl:
+		'https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption',
+};
